448f22334a7fce65a9bc006a2286408511672c03
jakegit93 <jakejohnsonbusiness@gmail.com>
2024-10-10 15:18:08 +1000

    updated chart stylings and font placement

--- tempAnomaly.js
+++ tempAnomaly.js
@@ -6,11 +6,59 @@ import data from './temperature-anomaly.json';
   new Chart(document.getElementById('tempAnomaly'), {
       type: 'line',
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        pointStyle: false,
+        tension: 0.5,
+        plugins: {
+          legend: {
+            display: false
+          },
+          title: {
+            display: true,
+            text: 'AVERAGE TEMPERATURE ANOMALY',
+            color: '#000000',
+            fullSize: false,
+            font: {
+              family: "Bebas Neue",
+              size: 25
+            },
+          }
+        },
         scales: {
           y: {
             title: {
               display: 'true',
-              text: 'Degrees Celcius',
+              text: 'DEGREES CELCIUS',
+              color: '#000000',
+              font: {
+                family: "Bebas Neue",
+                size: 25
+              },
+            },
+            ticks: {
+              color: '#000000'
+            },
+            grid: {
+              display: false,
+            }
+          },
+          x: {
+            title: {
+              text: 'YEAR',
+              display: true,
+              color: '#000000',
+              font: {
+                family: "Bebas Neue",
+                size: 25
+              }
+            },
+            ticks: {
+              color: '#000000',
+              font: {
+                family: "Bebas Neue",
+                size: 10
+              }
             }
           }
         }
